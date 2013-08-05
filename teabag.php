725835e3a4c76a9e9f06ba481348cb991dc5ebd6
email <ezkops@gmail.com>
2013-08-05 19:52:00 -0400

    if formula

---
+++ teabag.php
@@ -0,0 +1,9 @@
+    <?php
+        $teabags = 3;
+        
+        if ($teabags > 0) {
+          echo "There are tea bags! I'll have a cup!";
+        } else {
+          echo "No more tea! I guess I won't have a cup.";
+        }
+      ?>
